413c906cb7c8ea7de3988faec50f56b17e1f1c90
Philipp <philipp.raich@gmail.com>
2013-03-07 10:28:18 +0100

    typo

--- core/net/uip.h
+++ core/net/uip.h
@@ -2068,7 +2068,7 @@ CCIF extern uip_lladdr_t uip_lladdr;
   ((((a)->u8[8])  == (((m)->addr[0]) ^ 0x02)) &&   \
   */
 #define uip_is_addr_mac_addr_based(a, m) \
-  ((((a)->u8[8])  == ((m)->addr[0]) &&   \
+  ((((a)->u8[8])  == (m)->addr[0]) &&   \
    (((a)->u8[9])  == (m)->addr[1]) &&            \
    (((a)->u8[10]) == (m)->addr[2]) &&   \
    (((a)->u8[11]) == (m)->addr[3]) &&            \
