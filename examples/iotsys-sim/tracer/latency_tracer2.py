e5d1236df0ee67a98bf6e0b96f18b4cfb05298cd
Philipp Raich <philipp.raich@gmail.com>
2014-06-21 17:31:50 +0200

    update latency tracer for coaphttp round2

--- examples/iotsys-sim/tracer/latency_tracer2.py
+++ examples/iotsys-sim/tracer/latency_tracer2.py
@@ -36,46 +36,60 @@ def parse(filename, begin_pattern, end_pattern, requester, responders, interval=
 
 	startpoints = []
 	data = []
+	responders_left = list(responders)
 
 	endsfound = 0
 
 	lastend = 0
+	miss_count = 0
 
 	with open (filename, 'rb') as f :
 		reader = csv.reader (f, delimiter = '\t')
 		for row in reader :
-			if requester in row[2]:
-				if (p_begin.search(row[4])) :
+			if requester == int(row[2]) and (begin_pattern.search(row[4])) :
 					timestamp = int(row[1])
 					# print ("pot_start: " + str(timestamp))
 
 					if startpoints :
 						# print("laststart: " + str(startpoints[-1]))
 						# print("result: " + str(timestamp - startpoints[-1]))
-						# print("guard: "+ str(((interval * 1000) - 750)))
+						# print("guard: "+ str(((interval * 1000) - 3000)))
 						if (timestamp - int(startpoints[-1])) > ((interval * 1000) - 3000) :
-							# print ("PINNED end: " + str(lastend))
-							value = int(lastend) - startpoints[-1]
-							if value <= 0:
-								print "negative value not possible!"
-								print ("lastend: " + str(lastend) + " - startpoint: " +str(startpoints[-1]))
-								print ("current: " +str(timestamp))
-								print filename
-								exit(1)
+							if lastend > 0:
+								if len(responders_left) == 0:
+									# print ("PINNED end: " + str(lastend))
+									value = int(lastend) - startpoints[-1]
+									if value <= 0:
+										print "negative value not possible!"
+										print ("lastend: " + str(lastend) + " - startpoint: " +str(startpoints[-1]))
+										print ("current: " +str(timestamp))
+										print filename
+										exit(1)
 
-							data.append(value)
-							# add startpoint always (without end), remove again at the end
-							startpoints.append(timestamp)
-							# print ("PINNED start: " + str(timestamp))
+									# print "appending", value
+									data.append(value)
+									# reset responders_left
+									responders_left = list(responders)
+									# add startpoint always (without end), remove again at the end
+									startpoints.append(timestamp)
+									# print ("PINNED start: " + str(timestamp))
 
-							# break if enough data gathered
-							if len(data) == NR_OF_INTERVALS :
-								startpoints.pop()
-								break;
-						#else :
-						#	if not data :
-						#		print ("PINNED start (overwrite): " + str(timestamp))
-						#		startpoints[-1] = timestamp
+									# break if enough data gathered
+									if len(data) == NR_OF_INTERVALS :
+										startpoints.pop()
+										break
+
+								# we got a new startpoint, but have not yet reached all responders
+								else:
+									miss_count += 1
+									startpoints[-1] = timestamp
+									print "miss: resetting startpoint to", timestamp
+									print "resetting responders_left list"
+									responders_left = list(responders)
+
+							# else :
+							# 	print ("PINNED start (overwrite): " + str(timestamp))
+							# 	startpoints[-1] = timestamp
 
 
 					else :
@@ -84,20 +98,31 @@ def parse(filename, begin_pattern, end_pattern, requester, responders, interval=
 
 					# laststart = timestamp
 
-			for responder in responders:
-				# print ("responder: " +responder)
-				if responder in row[2] and requester in row[3]:
-					# print ("found match")
-					if (p_end.search(row[4])) :
+			# endpoints can happen when:
+			# - startpoint exists exists
+			# - begin_pattern occurs
+			# - recipient is one of the responders
+			if startpoints and not "-" in row[3] :
+				recipients = [int(x) for x in row[3].split(',')]
+				#print "recipients", recipients
+				if any(responder == int(row[2]) for responder in responders) and (requester in recipients) :
+					if (end_pattern.search(row[4])) :
+						# print ("found match")
+						current_responder = int(row[2])
 						endsfound += 1
-						timestamp = int(row[1])
-						if startpoints :
-							lastend = timestamp
-							# print("end: " + str(timestamp))
+						# this pattern might occur multiple times per request/response, thus we always overwrite the last one
+						lastend = int(row[1])
+						# print("end: " + str(timestamp))
+						# keep track of all responders, strike them out
+						if (current_responder in responders_left) :
+							# print "removing", current_responder
+							responders_left.remove(current_responder)
+						# else :
+							# print current_responder, "not in", responders_left
 
-		if not len(data) == NR_OF_INTERVALS :
-			# print ("PINNED end: " + str(lastend))
-			data.append(int(lastend) - startpoints[-1])
+	if not len(data) == NR_OF_INTERVALS :
+		# print ("PINNED end: " + str(lastend))
+		data.append(int(lastend) - startpoints[-1])
 
 	if len(startpoints) == 0 :
 		print("no startpoints found for " + filename + "!")
@@ -109,8 +134,7 @@ def parse(filename, begin_pattern, end_pattern, requester, responders, interval=
 
 	if len(data) != NR_OF_INTERVALS :
 		print("data size off: found "+ str(len(data)) +" intervals for " + filename + "!")
-		print filename
-		exit(1)
+		exit(0)
 
 	# print (startpoints[-1] - startpoints[0])
 	timespan = (startpoints[-1] - startpoints[0] + 500) / 1000;
@@ -160,12 +184,14 @@ conn = sqlite3.connect("coap.db")
 
 c = conn.cursor()
 
-#c.execute('''CREATE TABLE durations2 (binding text, hops int, payload int, requests int, duration int, iteration int, comments text)''')
+c.execute('''CREATE TABLE durations2 (binding text, hops int, payload int, requests int, duration int, iteration int, comments text)''')
 
 filenames = []
 
 for file in os.listdir("./"):
     if file.startswith("pdump-") and file.endswith(".txt"):
+    	if file.endswith("_bak.txt"):
+    		continue
     	filenames.append(file)
 
 for filename in filenames:
@@ -190,17 +216,28 @@ for filename in filenames:
 		req_interval = 10
 
 	if int(hop) == 1:
-		responder = ("2", "3", "4", "5")
+		if int(requests) == 1:
+			responder = [2]
+		elif int(requests) == 2:
+			responder = [2,3]
+		elif int(requests) == 4:
+			responder = [2,3,4,5]
 	elif int(hop) == 2:
-		if "http" in binding:
+		if int(requests) == 1:
+			responder = [7]
+		elif int(requests) == 2:
+			responder = [7,9]
+		elif int(requests) == 4:
+			responder = [6,7,8,9]
+
+	# override if http
+	if "http" in binding:
+		if int(hop) == 2:
 			if int(requests) == 1:
-				responder = ("3")
+				responder = [3]
 			elif int(requests) == 2:
-				responder = ("4", "5")
-			else:
-				responder = ("6", "7", "8", "9")
-		else:
-			responder = ("6", "7", "8", "9")
+				responder = [4,5]
+
 	comment = ""
 
 	if ("http" in binding):
@@ -211,10 +248,10 @@ for filename in filenames:
 		p_end = re.compile(coap64_end)
 
 	if req_interval != 10:
-		intervals[filename], mean_int_starts = parse(filename, p_begin, p_end, "1", responder, req_interval)
+		intervals[filename], mean_int_starts = parse(filename, p_begin, p_end, 1, responder, req_interval)
 		comment += (", "+ str(req_interval) +"s interval" if comment else ""+ str(req_interval) +"s interval")
 	else:
-		intervals[filename], mean_int_starts = parse(filename, p_begin, p_end, "1", responder)
+		intervals[filename], mean_int_starts = parse(filename, p_begin, p_end, 1, responder)
 
 	if mean_int_starts != 0:
 		comment_to_add = "racing clock (mean time between startpoints=" + str(mean_int_starts) + ")"
